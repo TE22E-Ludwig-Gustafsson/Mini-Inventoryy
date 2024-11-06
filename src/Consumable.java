f22f5479d51591bd50111bdd98acf6a553a8159d
Ludwig Gustafsson <ludwig.gustafsson@elev.ga.ntig.se>
2024-11-06 11:11:12 +0100

    klar med Consumables

--- src/Consumable.java
+++ src/Consumable.java
@@ -1,2 +1,20 @@
-public class Consumable {
+public class Consumable extends Item {
+    int useMax;
+    int usesCurrent;
+
+    public Consumable(String name, float weight, int useMax) {
+        super(name, weight);
+        this.useMax = useMax;
+        this.usesCurrent = useMax;
+    }
+
+    public void use(Character target){
+        if(usesCurrent > 0){
+            target.hp += 10;
+            usesCurrent--;
+            System.out.println(name + "used on " + target.name + ". " + target.name + "'s HP increased by 10.");
+        } else {
+            System.out.println(name + " has no uses left.");
+        }
+    }
 }
