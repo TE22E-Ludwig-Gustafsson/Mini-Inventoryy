53d2ee8e7895ab6b00f4af3e7aefaafa62ff4ac7
Ludwig Gustafsson <ludwig.gustafsson@elev.ga.ntig.se>
2024-11-13 10:46:00 +0100

    programmet fungerar så som det ska nu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
-import java.sql.SQLOutput;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Character player = new Character("Roffe", 100);
 
@@ -16,10 +17,23 @@ public class Main {
         //player.backpack.addItem(helmet);
 
         //Visa inventory
-        System.out.println("Start inventory");
+        System.out.println("Start väska");
         player.backpack.display();
 
+        //Visa spelaren en ny hjälm och fråga om den vill ta upp den
         Armor helmet = new Armor("Sigurts heliga hjälm", 2f,5);
+        System.out.println("\nDu hittade en legendarisk hjälm, vill du plocka up den? (Y/N): ");
+        String input = scanner.nextLine().trim().toLowerCase();
 
+        if (input.equals("y")) {
+            player.backpack.addItem(helmet);
+            System.out.println("Sigurts heliga hjälm är tillagd i din väska.");
+        }
+
+        System.out.println("\nUppdaterad väska:");
+        player.backpack.display();
+
+
+        scanner.close();
     }
 }
