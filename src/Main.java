0bd582c23d515e13908f5c14c4ad875aba51dfc5
Ludwig Gustafsson <ludwig.gustafsson@elev.ga.ntig.se>
2024-11-13 10:31:24 +0100

    ändrade Main lite

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,25 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
         Character player = new Character("Roffe", 100);
 
+        //Items som ska in i inventoryt
         Weapon tabbe = new Weapon("RallyTabben", 2f, 5, 100);
         Consumable potion = new Consumable("Lean", 0.5f,1);
-        Armor helmet = new Armor("Sigurts heliga hjälm", 2f,5);
 
+
+        //lägg till start items i inventory
         player.backpack.addItem(tabbe);
         player.backpack.addItem(potion);
-        player.backpack.addItem(helmet);
+        //player.backpack.addItem(helmet);
 
+        //Visa inventory
+        System.out.println("Start inventory");
+        player.backpack.display();
+
+        Armor helmet = new Armor("Sigurts heliga hjälm", 2f,5);
 
     }
 }
